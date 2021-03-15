de434c9f837adfc24258855cb7d621819ecde430
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-03-15 15:00:36 +0100

    Media

--- penultimate_clearance_system/urls.py
+++ penultimate_clearance_system/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from . import settings
 
 urlpatterns = [
     path('', include('clearance.urls')),
     path("accounts/", include("django.contrib.auth.urls")),
     path('admin/', admin.site.urls),
     path('api/', include('rest_framework.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
