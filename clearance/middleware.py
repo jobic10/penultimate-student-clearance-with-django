f9485ad4c17914ec3ca3a5ccbfd328abe608f261
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-03-18 22:26:09 +0100

    Fixed middleare bug

--- clearance/middleware.py
+++ clearance/middleware.py
@@ -9,13 +9,13 @@ class LoginCheckMiddleWare(MiddlewareMixin):
         user = request.user  # Who is the current user ?
         if user.is_authenticated:
             if user.user_type == '1':  # Is it the Admin
-                if modulename == 'portal.student_views' or modulename == 'portal.officer_views':
+                if modulename == 'clearance.student_views' or modulename == 'clearance.officer_views':
                     return redirect(reverse('admin_home'))
             elif user.user_type == '2':  # Officer :-/ ?
-                if modulename == 'portal.student_views' or modulename == 'portal.admin_views':
+                if modulename == 'clearance.student_views' or modulename == 'clearance.admin_views':
                     return redirect(reverse('officer_home'))
             elif user.user_type == '3':  # ... or Student ?
-                if modulename == 'portal.admin_views' or modulename == 'portal.officer_views':
+                if modulename == 'clearance.admin_views' or modulename == 'clearance.officer_views':
                     return redirect(reverse('student_home'))
             else:  # None of the aforementioned ? Please take the user to login page
                 return redirect(reverse('login_page'))
