0be604204a7b063b94e3ae23166ad5d4c1f16445
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-03-28 12:10:32 +0100

    Unused libraries removed

--- clearance/admin_views.py
+++ clearance/admin_views.py
@@ -5,7 +5,7 @@ from .forms import *
 from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
 from django.core.mail import send_mail
-import requests as r
+# import requests as r
 from django.template.loader import render_to_string
 from django.conf import settings
 # Create your views here.
